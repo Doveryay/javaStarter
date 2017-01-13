ffcb5fc874a1b84a55869501853e1819a5b39a18
Doveryay <doveryay@gmail.com>
2017-01-13 20:45:40 +0300

    Hello World

---
+++ src/egestats/HelloWorld.java
@@ -0,0 +1,10 @@
+package egestats;
+
+/**
+ * Created by Alexey on 13.01.17.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
