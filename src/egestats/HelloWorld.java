e9ceced9489e12fd3d6183c75b9d4cb6431a5861
butvin <butvan@gmail.com>
2017-01-16 20:42:16 +0300

    Loader implemented

--- src/egestats/HelloWorld.java
+++ src/egestats/HelloWorld.java
@@ -1,12 +1,14 @@
 package egestats;
 
+import java.io.IOException;
+
 /**
  * Main class for egestats.
  */
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World");
-        Loader loader = new Loader ("Peter");
+        Loader loader = new Loader("Peter");
         System.out.println(loader.load());
     }
 }
--- src/egestats/Loader.java
+++ src/egestats/Loader.java
@@ -1,18 +1,24 @@
 package egestats;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 /**
  * Loads html from website.
  */
 public class Loader {
+    private static final String SITE_URL = "https://cabinet.spbu.ru/Lists/1k_EntryLists/list_a9c0349c-00f4-4801-9bb3-80cecc0a53e8.html";
     private String name;
 
     public Loader(String name) {
         this.name = name;
     }
 
-    public String load() {
-        //todo Требуется реализация загрузки с сайта университета
-        throw new UnsupportedOperationException();
+    public String load() throws IOException {
+        java.io.InputStream response = new URL(SITE_URL).openStream();
+        Scanner s = new Scanner(response).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
     }
 
     public String getName() {
